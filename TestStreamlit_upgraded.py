37df1d11b0d5eb234207c06b5de6d085f7f17f89
mthai26 <mthai@ncsu.edu>
2026-04-29 13:01:54 -0400

    Update TestStreamlit_upgraded.py

--- TestStreamlit_upgraded.py
+++ TestStreamlit_upgraded.py
@@ -18,7 +18,7 @@ model = None
 try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-1.5-flash-latest")
 except Exception as e:
     st.error("API Key not found. Please set 'GEMINI_API_KEY' in Streamlit Secrets.")
 
